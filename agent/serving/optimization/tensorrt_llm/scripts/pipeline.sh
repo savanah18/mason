86cc4f81715d5fa8726b93b1d9aa448ccebd80d9
savanah18 <gpagluba@up.edu.ph>
2026-02-06 20:30:54 +0800

    Added pbtxt and env files

--- agent/serving/optimization/tensorrt_llm/scripts/pipeline.sh
+++ agent/serving/optimization/tensorrt_llm/scripts/pipeline.sh
@@ -1,5 +1,4 @@
-#!bin/bash
-
+echo "Converting model checkpoint to TensorRT LLM format..."
 python3 /app/tensorrt_llm/examples/models/core/qwen/convert_checkpoint.py \
     --model_dir $MODEL_PATH \
     --output_dir ${TENSORRT_LLM_CKPT} \
@@ -9,6 +8,7 @@ python3 /app/tensorrt_llm/examples/models/core/qwen/convert_checkpoint.py \
     --weight_only_precision ${QUANTIZATION_TYPE}
 
 
+echo "Converting model checkpoint to TensorRT LLM format..."
 trtllm-build --checkpoint_dir ${TENSORRT_LLM_CKPT}\
         --gpt_attention_plugin ${DTYPE}  \
         --remove_input_padding enable \
@@ -18,7 +18,7 @@ trtllm-build --checkpoint_dir ${TENSORRT_LLM_CKPT}\
 
 python3 /app/tensorrt_llm/examples/generate_xgrammar_tokenizer_info.py \
     --model_dir ${MODEL_PATH} \
-    --output_dir ${TENSORRT_ENGINE_PATH}
+    --output_dir ${TENSORRT_ENGINE_PATH}/tokenizer
 
 
 
